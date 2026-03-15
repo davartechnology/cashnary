15ebf1fd03ad1d9c2ebda29ee6512d31dd5b7839
daveonlymaster <daveonlymaster@gmail.com>
2026-03-15 06:10:01 -0400

    fix: next.config.mjs ES module compatible

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  images: {
-    remotePatterns: [],
-  },
-}
-
-module.exports = nextConfig
