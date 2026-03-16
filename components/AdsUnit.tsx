b2855de975e3cea342284a93a8c6d5f2cc3f9ba8
daveonlymaster <daveonlymaster@gmail.com>
2026-03-16 15:38:19 -0400

    fix: nouveau code A-Ads 2430703

--- components/AdsUnit.tsx
+++ components/AdsUnit.tsx
@@ -2,8 +2,8 @@ export default function AdsUnit() {
   return (
     <div style={{ width: '100%', margin: '20px auto', position: 'relative', zIndex: 99998 }}>
       <iframe 
-        data-aa='2430696' 
-        src='//acceptable.a-ads.com/2430696/?size=Adaptive&background_color=0A0A0A&title_color=D4AF37&text_color=FFFFFF&link_color=9CA3AF'
+        data-aa='2430703' 
+        src='//acceptable.a-ads.com/2430703/?size=Adaptive'
         style={{ 
           border: 0, 
           padding: 0, 
