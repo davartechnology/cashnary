2643add63b7241a4e6bddf8639721797036a5abe
daveonlymaster <daveonlymaster@gmail.com>
2026-03-16 14:58:54 -0400

    fix: AdsUnit déplacé à la racine components/

---
+++ components/AdsUnit.tsx
@@ -0,0 +1,19 @@
+export default function AdsUnit() {
+  return (
+    <div style={{ width: '100%', margin: '20px auto', position: 'relative', zIndex: 99998 }}>
+      <iframe 
+        data-aa='2430696' 
+        src='//acceptable.a-ads.com/2430696/?size=Adaptive&background_color=0A0A0A&title_color=D4AF37&text_color=FFFFFF&link_color=9CA3AF'
+        style={{ 
+          border: 0, 
+          padding: 0, 
+          width: '70%', 
+          height: 'auto', 
+          overflow: 'hidden', 
+          display: 'block', 
+          margin: 'auto' 
+        }}
+      />
+    </div>
+  )
+}
